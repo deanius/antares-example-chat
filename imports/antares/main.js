330750df1a0f2dededa9e57bdd15693aeada4d20
Dean Radcliffe <deanmisc@yahoo.com>
2017-02-01 20:54:07 -0600

    chore: Update to Meteor 1.4.3.1

--- imports/antares/main.js
+++ imports/antares/main.js
@@ -1,15 +1,19 @@
 import { AntaresMeteorInit, AntaresInit, inAgencyRun } from 'meteor/deanius:antares'
 
-// Build up a config object, via imports
-const AntaresConfig = {}
+// TODO: Build up a config object, via imports
+let Actions = {}
+const AntaresConfig = {
+    Actions
+}
 
 // Pass the config to the meteorized version of AntaresInit
 export const Antares = AntaresMeteorInit(AntaresInit)(AntaresConfig)
 
-// In 'any' agent expose a top-level Antares global for demo purposes
+// Example: In 'any' agent expose a top-level Antares globals for demo purposes
 inAgencyRun('any', function () {
     Object.assign(this, {
-        Antares
+        Antares,
+        announce: Antares.announce,
+        log: console.log.bind(console)
     })
-    // TODO define startup code here
 })
