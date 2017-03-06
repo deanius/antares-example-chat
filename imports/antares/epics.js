f0a0501ff4e04e1770683c0aedd0179b2e6e95a1
Dean Radcliffe <deanmisc@yahoo.com>
2017-03-06 15:56:30 -0600

    feat: typing indicator part 3.1: dismiss typing notification attempt 1
    This one has a bug: new typing notifications do not extend the life of
    the typing indicator. It should be dismissed 2500 msec after the
    last-received typing indicator, not after the first one.
    msec   action
    ----    ------
    0 Other 1 types
    50 Self gets typing indicator, sets dismissal for 2550
    1500 Other 2 types again
    1550 Self gets new typing indicator
    
    The typing indicator will be dismissed at 2550 - the original value -
    instead of 4050, the new value.

--- imports/antares/epics.js
+++ imports/antares/epics.js
@@ -1,12 +1,31 @@
 import { originate } from './main'
 import Actions from './actions'
+import Rx from 'rxjs'
 
 export default {
     notifyOfTyping: action$ =>
         action$.ofType('Activity.type')
-            .throttleTime(2000)
+            .throttleTime(1000)
             .map(a =>
                 originate(Actions.Activity.notifyOfTyping, {
                     sender: a.payload.sender
-                }))
+                })),
+    
+    removeTypingNotification: action$ =>
+        action$.ofType('Activity.notifyOfTyping')
+            .filter(a => a.payload.active === true)
+            .mergeMap(notificationOnAction =>
+                Rx.Observable.timer(2500)
+                    .map(() => ({
+                        type: 'Activity.notifyOfTyping',
+                        payload: {
+                            active: false,
+                            sender: notificationOnAction.payload.sender
+                        },
+                        meta: {
+                            antares: {
+                                localOnly: false
+                            }
+                        }
+                    })))
 }
