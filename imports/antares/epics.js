14a3e8f74616c4d2c4a60fc58c501f0e6e70aedf
Dean Radcliffe <deanmisc@yahoo.com>
2017-03-06 15:19:18 -0600

    feat: typing indicator part 2.2: And throttle frequency to 2 sec

--- imports/antares/epics.js
+++ imports/antares/epics.js
@@ -4,6 +4,7 @@ import Actions from './actions'
 export default {
     notifyOfTyping: action$ =>
         action$.ofType('Activity.type')
+            .throttleTime(2000)
             .map(a =>
                 originate(Actions.Activity.notifyOfTyping, {
                     sender: a.payload.sender
