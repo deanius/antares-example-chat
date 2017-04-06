7716df3462e8abbb1848a7610ad2525404a04c48
Dean Radcliffe <deanmisc@yahoo.com>
2017-04-05 21:51:43 -0500

    chore: Load Antares on client and server
    > meteor add deanius:antares
    
    Add import/antares/index, client/index, server/index
    
    Verify:
    * the store is visible in Redux DevTools with a top-level key
    'antares'
    * Antares.announce from client
    * Antares.announce from server

---
+++ imports/antares/index.js
@@ -0,0 +1,17 @@
+import { AntaresMeteorInit, AntaresInit, inAgencyRun } from 'meteor/deanius:antares'
+import { Observable } from 'meteor/deanius:antares'
+
+// Build up a config object, via imports
+const AntaresConfig = {}
+
+// Pass the config to the meteorized version of AntaresInit
+export const Antares = AntaresMeteorInit(AntaresInit)(AntaresConfig)
+
+// In 'any' agent expose a top-level Antares global for demo purposes
+inAgencyRun('any', function () {
+    Object.assign(this, {
+        Antares,
+        Observable
+    })
+    // TODO define startup code here
+})
