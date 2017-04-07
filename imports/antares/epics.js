f517d275d6d05ca84586620ffcce06f854db8b49
Dean Radcliffe <deanmisc@yahoo.com>
2017-04-07 13:45:25 -0500

    feat: typing indicator is dismissed by the earlier of a new message, OR a 2500 msec timeout Verify:   - either message send OR timeout dismisses   - indicator can be "kept alive" as long as Activity.type actions not throttled away   - only single Activity.notifyOfTyping(active:false) emitted in either case

--- imports/antares/epics.js
+++ imports/antares/epics.js
@@ -1,28 +1,46 @@
 import Actions from './actions'
+import { Observable } from 'meteor/deanius:antares'
 
 export default {
     notifyOfTyping: action$ =>
         action$.ofType('Activity.type')
-            .throttleTime(2000)
+            .throttleTime(1000)
             .map(a => Actions.Activity.notifyOfTyping({
                 sender: a.payload.sender
             })),
 
     dismissTypingV1: action$ => {
-        const dismissUponMessageFromSame = notifyAction =>
+        // Creates an action that cancels typingIndication for that sender,
+        //  but is not broadcast to other nodes
+        const createCancelActionFor = typingSender => ({
+            type: 'Activity.notifyOfTyping',
+            payload: {
+                sender: typingSender,
+                active: false
+            },
+            meta: { antares: { localOnly: true } }
+        })
+
+        // Given a senders Id, returns an Observable of cancellations
+        //  triggered by a received message from that same sender
+        const dismissUponMessageFrom = typingSender =>
             action$.ofType('Message.send')
-                .filter(a => a.payload.sender === notifyAction.payload.sender)
-                .mapTo({
-                    type: 'Activity.notifyOfTyping',
-                    payload: {
-                        ...notifyAction.payload,
-                        active: false
-                    },
-                    meta: { antares: { localOnly: true } }
-                })
+                .filter(newMsgAction => newMsgAction.payload.sender === typingSender)
+                .mapTo(createCancelActionFor(typingSender))
+
+        // Given a senders Id, returns an observable that in 2500 msec
+        //   emits an action to turn the indicator off for that sender
+        const timeoutIndicator = typingSender =>
+            Observable.timer(2500)
+                .mapTo(createCancelActionFor(typingSender))
 
         return action$.ofType('Activity.notifyOfTyping')
             .filter(a => a.payload.active === true)
-            .mergeMap(dismissUponMessageFromSame)
+            .switchMap(notifyAction =>
+                Observable.race(
+                    dismissUponMessageFrom(notifyAction.payload.sender),
+                    timeoutIndicator(notifyAction.payload.sender)
+                )
+            )
     }
 }
