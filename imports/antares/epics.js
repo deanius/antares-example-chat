03162130567e123c85ab1382cd776393bc2372bb
Dean Radcliffe <deanmisc@yahoo.com>
2017-04-06 20:29:31 -0500

    feat: Typing indicator is dismissed upon message from same sender
    Verify:
     - Typing shows indicator, message send dismisses
     - bug: note that too many notifyOfTyping(active:false) are emitted due
       to mergeMap instead of switchMap

--- imports/antares/epics.js
+++ imports/antares/epics.js
@@ -6,5 +6,23 @@ export default {
             .throttleTime(2000)
             .map(a => Actions.Activity.notifyOfTyping({
                 sender: a.payload.sender
-            }))
+            })),
+
+    dismissTypingV1: action$ => {
+        const dismissUponMessageFromSame = notifyAction =>
+            action$.ofType('Message.send')
+                .filter(a => a.payload.sender === notifyAction.payload.sender)
+                .mapTo({
+                    type: 'Activity.notifyOfTyping',
+                    payload: {
+                        ...notifyAction.payload,
+                        active: false
+                    },
+                    meta: { antares: { localOnly: true } }
+                })
+
+        return action$.ofType('Activity.notifyOfTyping')
+            .filter(a => a.payload.active === true)
+            .mergeMap(dismissUponMessageFromSame)
+    }
 }
