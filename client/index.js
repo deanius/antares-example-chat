39ccc77f8f971c011a16f7a9b67ad28460e40f7a
Dean Radcliffe <deanmisc@yahoo.com>
2017-04-24 15:33:31 -0500

    feat: subscribe scrollToBottom renderer

--- client/index.js
+++ client/index.js
@@ -3,7 +3,7 @@ import React from 'react'
 import { render } from 'react-dom'
 import { LiveChat } from '/imports/components/LiveChat'
 import { Meteor } from 'meteor/meteor'
-import { store, subscribe } from '/imports/antares'
+import { Antares, store, subscribe } from '/imports/antares'
 
 Meteor.startup(() => {
     render(<LiveChat store={store} />, document.getElementById('react-root'))
@@ -11,3 +11,12 @@ Meteor.startup(() => {
     subscribe({ key: ['chats', 'chat:demo'] })
     // Since we have only one key, this is like subscribe('*')
 })
+
+// A renderer is a function, which, given action, state and diffs
+// may choose to run a side effect
+let scrollToBottom = ({ action }) => {
+    if (action.type === 'Message.send') {
+        window.scrollTo(0, document.body.scrollHeight)
+    }
+}
+Antares.subscribeRenderer(scrollToBottom)
