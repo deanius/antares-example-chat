0320f627dd8149e9f5ec0b8225284998132f09bc
Dean Radcliffe <deanmisc@yahoo.com>
2017-04-26 06:44:42 -0500

    Simplify mongoRenderer

--- server/index.js
+++ server/index.js
@@ -10,22 +10,11 @@ Object.assign(global, {
     Actions
 })
 
-// On the server, where Collections.Foo.update would make sense,
-// write to the database. This is roughly the source of
-// Antares.mongoRendererFor(Collections)
-Antares.subscribeRenderer(({ action, mongoDiff }) => {
-    // First- save the action!
-    if (!action.type.startsWith('Activity.')) {
-        Actions.insert(action)
-    }
-
-    // The mongoDiff object, available after every action, contains enough information to
-    // apply the reducer's diffs to a Mongo database.. First we pluck off those interesting fields.
-    if (mongoDiff) {
-        let { id, updateOp, upsert } = mongoDiff
-
-        console.log('MDB> ', updateOp)
-        // Do the actual imperative update to the database, and handle exceptions..
-        Chats.update(id, updateOp, upsert)
-    }
-})
+// On the server, where Collections.Foo.update would make sense, write to the database.
+const mongoRenderer = ({ action, mongoDiff }) => {
+    if (!mongoDiff) return
+	let { id, updateOp, upsert } = mongoDiff
+	console.log('MDB> ', updateOp)
+	Chats.update(id, updateOp, upsert)
+}
+Antares.subscribeRenderer(mongoRenderer)
