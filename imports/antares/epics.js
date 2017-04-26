cf85c0ecde341a11bb614d9587294caeb144b10e
Dean Radcliffe <deanmisc@yahoo.com>
2017-04-26 13:54:17 -0500

    rephrase

--- imports/antares/epics.js
+++ imports/antares/epics.js
@@ -1,6 +1,30 @@
 import Actions from './actions'
 import { Observable } from 'meteor/deanius:antares'
 
+// Creates an action that cancels typingIndication for that sender,
+//  but is not broadcast to other nodes
+const createCancelActionFor = typingSender => ({
+    type: 'Activity.notifyOfTyping',
+    payload: {
+        sender: typingSender,
+        active: false
+    },
+    meta: { antares: { localOnly: true } }
+})
+
+// Given a senders Id, returns an Observable which emits a
+// typing cancellation action when a message comes from that sender
+const dismissUponMessageFrom = typingSender =>
+    action$.ofType('Message.send')
+        .filter(newMsgAction => newMsgAction.payload.sender === typingSender)
+        .mapTo(createCancelActionFor(typingSender))
+
+// Given a senders Id, returns an observable that in 2500 msec
+//   emits an action to turn the indicator off for that sender
+const timeoutIndicator = typingSender =>
+    Observable.timer(2500)
+        .mapTo(createCancelActionFor(typingSender))
+
 export default {
     notifyOfTyping: action$ =>
         action$.ofType('Activity.type')
@@ -10,30 +34,6 @@ export default {
             })),
 
     dismissTypingV1: action$ => {
-        // Creates an action that cancels typingIndication for that sender,
-        //  but is not broadcast to other nodes
-        const createCancelActionFor = typingSender => ({
-            type: 'Activity.notifyOfTyping',
-            payload: {
-                sender: typingSender,
-                active: false
-            },
-            meta: { antares: { localOnly: true } }
-        })
-
-        // Given a senders Id, returns an Observable of cancellations
-        //  triggered by a received message from that same sender
-        const dismissUponMessageFrom = typingSender =>
-            action$.ofType('Message.send')
-                .filter(newMsgAction => newMsgAction.payload.sender === typingSender)
-                .mapTo(createCancelActionFor(typingSender))
-
-        // Given a senders Id, returns an observable that in 2500 msec
-        //   emits an action to turn the indicator off for that sender
-        const timeoutIndicator = typingSender =>
-            Observable.timer(2500)
-                .mapTo(createCancelActionFor(typingSender))
-
         return action$.ofType('Activity.notifyOfTyping')
             .filter(a => a.payload.active === true)
             .switchMap(notifyAction =>
