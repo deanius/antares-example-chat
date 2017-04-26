aa4ebcd056b27f8b1c2b013379ac6ade146cb192
Dean Radcliffe <deanmisc@yahoo.com>
2017-04-26 06:46:49 -0500

    feat: Send messages with SMS to twilio

--- server/index.js
+++ server/index.js
@@ -10,6 +10,8 @@ Object.assign(global, {
     Actions
 })
 
+// On the server, where Collections.Foo.update would make sense,
+// write to the database.
 // On the server, where Collections.Foo.update would make sense, write to the database.
 const mongoRenderer = ({ action, mongoDiff }) => {
     if (!mongoDiff) return
@@ -18,3 +20,24 @@ const mongoRenderer = ({ action, mongoDiff }) => {
 	Chats.update(id, updateOp, upsert)
 }
 Antares.subscribeRenderer(mongoRenderer)
+
+
+const twilioRenderer = ({ action }) => {
+    console.log('Sending to twilio', action.payload.message)
+    const message = action.payload.message
+    const twilio = require('twilio')
+    const client = new twilio.RestClient(process.env.DEMO_TWILIO_SID, process.env.DEMO_TWILIO_TOKEN);
+
+    client.messages.create({
+        body: message,
+        to: process.env.DEMO_TWILIO_TO,
+        from: process.env.DEMO_TWILIO_FROM
+    }, (err, r) => console.log({ err, r }))
+}
+
+Antares.subscribeRenderer(twilioRenderer, {
+    mode: 'async',
+    xform: action$ => action$.filter(({ action }) =>
+        action.type === 'Message.send' && action.payload.message.includes('SMS')
+    )
+})
